4cdda23a831a93d6b8da3de746a9c0fb1f303120
naveenmovva <naveenmovva225@gmail.com>
2018-04-19 13:33:27 -0700

    commit after creating Tax.java

--- src/com/JavaProgramming24HourTrainer/lesson3/Tax.java
+++ src/com/JavaProgramming24HourTrainer/lesson3/Tax.java
@@ -1,5 +1,14 @@
 package com.JavaProgramming24HourTrainer.lesson3;
 
 public class Tax {
+	double grossIncome;
+	String state;
+	int dependents;
+	public double calculateTax(){
+		return 234.95;
+	}
+	public static void main(String [] args) {
+		System.out.println("testing");
+	}
 
 }
